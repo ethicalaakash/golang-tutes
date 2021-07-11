50a3b1cab51f69ef88a7fa0d15328ed2355348c6
Aakash Singhal <ethicalaakash@gmail.com>
2021-07-11 12:42:24 +0530

    Iterate over map

--- maps/main.go
+++ maps/main.go
@@ -6,12 +6,17 @@ import (
 
 func main() {
 	// var color map[string]string Another way of declaring a map
-	colors := make(map[string]string)
-	// colors := map[string]string{
-	// 	"red":  "34bkjvb",
-	// 	"blue": "askjfh3",
-	// }
-	colors["white"] = "fffff"
-	delete(colors, "white")
-	fmt.Println(colors)
+	// colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "34bkjvb",
+		"blue":  "askjfh3",
+		"white": "ffff",
+	}
+
+	printMap(colors)
+}
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("hex coode for", color, "is", hex)
+	}
 }
